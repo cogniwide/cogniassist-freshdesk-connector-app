76f301c4858202caf9b2c82f1552d22cb3052ab2
Alfred Francis <alfredfranciz@gmail.com>
2020-11-07 13:51:14 +0530

    Initial commit with working app

---
+++ README.md
@@ -0,0 +1,55 @@
+## CogniAssist Connector
+Plugin to forward New tickets and Conversation updates to CogniAssist Platform
+
+### Overview
+
+Manage conversations, build relationships and serve your customers and partners better, all from one place.
+
+Salient Features
+
+Go live with CogniAssist in less time using CogniConnect
+==============================================
+CogniConnect is Cogniwide's API builder and manager, a low code API
+management platform that makes API creation and integrations faster.
+
+
+Build Trust using Explainable Al Feature
+==============================================
+Evidences that throw light on lineage, intent and logic helps trouble
+shooters, audit teams and regulators.
+
+Transfer to live agent using CogniChat
+==============================================
+Customer Emotion & Sentiment based auto-transfer to live agent from
+Digital Assistant using CogniChat ensures seamless connectivity.
+
+One powerful dashboard to engage your customers
+==============================================
+Get Real-time reports, monitor and respond better than ever.
+
+State of the Art Security
+==============================================
+Choose robust SAAS infrastructure with five layers of cloud security or
+on-prem hosting of Digital Assistant  
+
+### Project folder structure explained
+
+    .
+    ├── README.md                  This file.
+    ├── config                     Installation parameter configs.
+    │   ├── iparams.json           Installation parameter config in English language.
+    │   └── iparam_test_data.json  Installation parameter data for local testing.
+    └── manifest.json              Project manifest.
+    └── server                     Business logic for remote request and event handlers.
+        ├── lib
+        │   └── utils.js
+        ├── server.js
+        └── test_data
+            ├── onAppInstall.json
+            ├── onAppUninstall.json
+            ├── onContactCreate.json
+            ├── onContactUpdate.json
+            ├── onConversationCreate.json
+            ├── onExternalEvent.json
+            ├── onTicketCreate.json
+            └── onTicketUpdate.json
---
+++ server/server.js
@@ -0,0 +1,96 @@
+
+/**  
+ * @description - Everytime a new ticket is created/updated, this app makes an API 
+ * request to CogniAssist and prints the response to the terminal window.
+ */
+var request = require('request');
+var utils = require('./lib/utils');
+var base64 = require('base-64');
+
+exports = {
+
+  events: [
+    { event: 'onTicketCreate', callback: 'onTicketCreateHandler' },
+    { event: "onConversationCreate", callback: "onConversationCreateCallback" }
+  ],
+
+  // args is a JSON block containing the payload information.
+  // args['iparam'] will contain the installation parameter values.
+
+  onTicketCreateHandler: function (args) {
+
+    var responder_id = args['data']['ticket']["responder_id"]
+
+    // dont call webhook if ticket has already an assignee other than bot user
+    if (responder_id != null && responder_id != args['iparams']["botUserID"])
+      return;
+
+    var payload = {
+      "body_text": args['data']['ticket']['description_text'],
+      "body_html": args['data']['ticket']['description'],
+      "ticket_id": args['data']['ticket']['id'],
+      "event": "ticket_created",
+      "meta": {
+        "attachments": args['data']['ticket']['attachments']
+      }
+    }
+    var headers = { "Authorization": "Bearer " + args['iparams']["cogniassistAPIKey"] };
+    var options = { method: "POST", headers: headers, json: true, body: payload };
+    var url = args['iparams']["cogniassistInstanceURL"] + "webhooks/freshdesk/webhook";
+
+    // call CogniAssist webhook with ticket data
+    request(url, options, (error, response) => {
+      utils.handleResponse(error, response);
+    });
+  },
+
+  onConversationCreateCallback: function (args) {
+
+    // dont call webhook if reply is created by agent
+    if (!args['data']['conversation']['incoming'])
+      return;
+
+    var freshdeskAPIURL = "https://"+args['iparams']["freshdeskDomain"]+".freshdesk.com/api/v2/tickets/" + args['data']['conversation']['ticket_id']
+    var requestOptions = {
+      method: 'GET',
+      headers: {
+        Authorization: "Basic " +  base64.encode(args['iparams']["freshdeskAPIKey"])
+      },
+      json:true
+    }
+
+    request(freshdeskAPIURL,requestOptions, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          console.log(body)
+          // ignore if reply is not to a ticket handled by BOT user
+          if(body["responder_id"] != args['iparams']["botUserID"]){
+            return;
+          }
+
+          var payload = {
+            "body_text": args['data']['conversation']['body_text'],
+            "body_html": args['data']['conversation']['body'],
+            "ticket_id": args['data']['conversation']['ticket_id'],
+            "event": "reply_created",
+            "meta": {
+              "attachments": args['data']['conversation']['attachments']
+            }
+          }
+
+          var headers = { "Authorization": "Bearer " + args['iparams']["cogniassistAPIKey"] };
+          var options = { method: "POST", headers: headers, json: true, body: payload };
+          var url = args['iparams']["cogniassistInstanceURL"] + "webhooks/freshdesk/webhook";
+
+          // call CogniAssist webhook with ticket data
+          request(url, options, (error, response) => {
+            utils.handleResponse(error, response);
+          });
+
+        }else{
+          console.log(error)
+        }
+      }
+    )
+  }
+
+};
